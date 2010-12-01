dd4e517006a536ac2509e5ca8a8009a78dcf7eb3
Simple Geebus <nerds+geebus@simplegeo.com>
2010-12-01 20:00:18 +0000

    Prepared changelog for build 12

--- _version.py
+++ _version.py
@@ -1,7 +1,6 @@
 # This is the version of this source code.
 
 verstr = "1.4"
-try:
     from pyutil.version_class import Version as pyutil_Version
     __version__ = pyutil_Version(verstr)
 except (ImportError, ValueError):
